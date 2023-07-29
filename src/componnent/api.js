6e6901760fbbcaa5edf137c6ad91d01e5b1187dc
Pushpender <Pushpenderjangra2005@gmail.com>
2023-07-30 00:23:07 +0530

    Added responsive for laptop

--- src/componnent/api.js
+++ src/componnent/api.js
@@ -3,12 +3,8 @@ const Api = async (url) => {
     const options = {
         method: 'GET',
         headers: {
-            // 89fdae862amsh2dfee50e62e6d2ep1e327cjsn4f11a08b9a48
-            //f0fc9f94c0msh157922b23854ec6p109ad4jsne37266475397
-            //6162fb09d2msh373573371aa17e9p1b9acfjsn0871fe26b207
-            // f0fc9f94c0msh157922b23854ec6p109ad4jsne37266475397
-            // 9ff6d793f6msh845e2fb244133f3p185f31jsn1a3f5a3f61bb
-            'X-RapidAPI-Key': '4563446d26mshdeff2f581c3b046p14ca36jsn402ceb88dd88',
+
+            'X-RapidAPI-Key': 'Your API KEY',
             'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
         }
     };
